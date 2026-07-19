0e766af7ee7618b141ac407b51ef65419be2bf96
samantasubedi <samantasubedi01@gmail.com>
2026-07-19 11:52:34 +0545

    initializing project

---
+++ backend/src/index.ts
@@ -0,0 +1,5 @@
+import { Elysia } from "elysia";
+
+const app = new Elysia().get("/", () => "Hello Elysia").listen(4000);
+
+console.log(`Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
